7083412566bc6dbe4aa445c755b0d78ffbd7f6e0
Manikanta148 <adabalamanikanta2004@gmail.com>
2025-09-22 09:12:21 +0530

    Add files via upload
    Here we control GPIOs(eg.LED) by using web page with simple ON and OFF buttons.ESP32 acts as a web server.We control the LED from anywhere by using Firebase RealTime Database.When we press the buttons on the webpage the led changes it's state.

---
+++ ControlGPIOs/ControlGPIOs.ino
@@ -0,0 +1,85 @@
+#include <WiFi.h>
+#include <FirebaseESP32.h>
+
+// Provide the token generation process info.
+#include <addons/TokenHelper.h>
+
+// Provide the RTDB payload printing info and other helper functions.
+#include <addons/RTDBHelper.h>
+
+#define WIFI_SSID "moto g35 5G_8995"
+#define WIFI_PASSWORD "moto12345"
+
+#define API_KEY "AIzaSyAsbeuPd7mNrALj8yu9tx85eSAjyq58Roo"
+
+#define DATABASE_URL "https://controlgpios-default-rtdb.firebaseio.com/"
+
+#define USER_EMAIL "beginnercoder2004@gmail.com"
+#define USER_PASSWORD "coder@2004"
+
+#define LED_PIN 18
+
+FirebaseData fbdo;
+FirebaseAuth auth;
+FirebaseConfig config;
+
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(LED_PIN,OUTPUT);
+  Serial.begin(115200);
+  WiFi.begin(WIFI_SSID,WIFI_PASSWORD);
+  Serial.print("Connecting to Wifi.");
+  while(WiFi.status() != WL_CONNECTED){
+    delay(300);
+    Serial.print(".");
+  }
+  Serial.println();
+  Serial.print("Connected with IP: ");
+  Serial.println(WiFi.localIP());
+
+  /* Assign the api key (required) */
+  config.api_key = API_KEY;
+
+  /* Assign the user sign in credentials */
+  auth.user.email = USER_EMAIL;
+  auth.user.password = USER_PASSWORD;
+
+  /* Assign the RTDB URL (required) */
+  config.database_url = DATABASE_URL;
+
+   /* Assign the callback function for the long running token generation task */
+  config.token_status_callback = tokenStatusCallback; // see addons/TokenHelper.h
+
+
+  // Comment or pass false value when WiFi reconnection will control by your code or third party library e.g. WiFiManager
+  Firebase.reconnectNetwork(true);
+
+  // Since v4.4.x, BearSSL engine was used, the SSL buffer need to be set.
+  // Large data transmission may require larger RX buffer, otherwise connection issue or data read time out can be occurred.
+  fbdo.setBSSLBufferSize(4096 /* Rx buffer size in bytes from 512 - 16384 */, 1024 /* Tx buffer size in bytes from 512 - 16384 */);
+
+  // Limit the size of response payload to be collected in FirebaseData
+  fbdo.setResponseSize(2048);
+
+  Firebase.begin(&config, &auth);
+
+  Firebase.setDoubleDigits(5);
+
+  config.timeout.serverResponse = 10 * 1000;
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  if (Firebase.getBool(fbdo, "/led/state")) {
+    if (fbdo.dataType() == "boolean") {
+      bool ledState = fbdo.boolData();
+      digitalWrite(LED_PIN, ledState ? HIGH : LOW);
+      Serial.println(ledState ? "LED ON" : "LED OFF");
+    }
+  } else {
+    Serial.print("Firebase get failed: ");
+    Serial.println(fbdo.errorReason());
+  }
+
+  delay(1000); // check every second)
+}
